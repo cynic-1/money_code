03eed636bf9d16ca867c74bea2d89462a4cb7661
Cynic <cynic2001@gmail.com>
2024-01-23 21:29:06 +0800

    add: log to file

--- utils/logger.py
+++ utils/logger.py
@@ -5,6 +5,6 @@ class Logger:
     log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
     @staticmethod
-    def get_logger(name=__name__, level=logging.INFO):
-        logging.basicConfig(level=level, format=Logger.log_format)
+    def get_logger(name=__name__, level=logging.DEBUG):
+        logging.basicConfig(level=level, filename='mexc.log', format=Logger.log_format)
         return logging.getLogger(name)
