9ca144011c01b133266a0a37cee81381e58fcde1
crypto_jedi <ybn1992@gmail;.com>
2024-01-23 18:58:04 +0800

    add migration

---
+++ migrations/up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS prices_8h
+    (
+        symbol                TEXT                            NOT NULL,
+        exchange              TEXT                            NOT NULL,
+        timestamp             BIGINT                          NOT NULL, 
+        open                  NUMERIC                         NOT NULL,
+        high                  NUMERIC                         NOT NULL,
+        low                   NUMERIC                         NOT NULL,
+        close                 NUMERIC                         NOT NULL,
+        vbtc                  NUMERIC                         NOT NULL,    
+        usd_ema_12            NUMERIC,
+        usd_ema_144           NUMERIC,
+        usd_ema_169           NUMERIC,
+        usd_ema_576           NUMERIC,
+        usd_ema_676           NUMERIC,
+        btc_ema_12            NUMERIC,
+        btc_ema_144           NUMERIC,
+        btc_ema_169           NUMERIC,
+        btc_ema_576           NUMERIC,
+        btc_ema_676           NUMERIC
+    );
+    CREATE INDEX IF NOT EXISTS ex_symbol_time on prices_8h (exchange, symbol, timestamp);
+    CREATE INDEX IF NOT EXISTS symbol_time ON prices_8h (timestamp, symbol);
