b15598457d43f1f1ad6b06928f79d782d30cec01
Cynic <cynic2001@gmail.com>
2024-02-29 18:51:11 +0800

    chore: change log name

--- utils/logger.py
+++ utils/logger.py
@@ -6,5 +6,5 @@ class Logger:
 
     @staticmethod
     def get_logger(name=__name__, level=logging.DEBUG):
-        logging.basicConfig(level=level, filename='fuck.log', format=Logger.log_format, encoding='utf-8')
+        logging.basicConfig(level=level, filename='cex.log', format=Logger.log_format, encoding='utf-8')
         return logging.getLogger(name)
